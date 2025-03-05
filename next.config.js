2e521c5d99734fa8365da892b9a3991ee3ff54d6
safal25 <safalkhampariya80@gmail.com>
2025-03-05 06:34:46 -0600

    Made config changes to fix github pages issue

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    basePath: '/activity-manager',
+    assetPrefix: '/activity-manager/',
+}
 
 module.exports = nextConfig
